aade00bcc34a6869592ddcab472fff101e7b665c
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-05-02 23:25:33 +0300

    feat: fade-out-animation & transform-animation

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -55,17 +55,25 @@ const openFullSizePageWithCard = event => {
   // MEMO: this must be preceded before rippleAnimation();
   toggleFullSizePageWithCard();
 
-  // MEMO: ripple animation
-  const gesture = {
-    x: event.x || event.pageX,
-    y: event.y || event.pageY
-  };
-  runRippleAnimation({gesture, from: event.target, to: fixedBackground});
+  runRippleAnimation({
+    gesture: {
+      x: event.x || event.pageX,
+      y: event.y || event.pageY
+    },
+    from: event.target,
+    to: fixedBackground
+  });
   runHeroAnimation({delay: 150, from: event.target, to: card});
 };
 
 const closeFullSizePageWithCard = event => {
-  toggleFullSizePageWithCard();
+  runFadeOutAnimation({node: fixedBackground});
+
+  runTransformAnimation({
+    transformFrom: "none",
+    transformTo: "translate(0px,-200vh) scale(0.9,1)",
+    node: card
+  });
 };
 
 const toggleFullSizePageWithCard = () => {
@@ -101,12 +109,10 @@ const runRippleAnimation = ({ gesture, from, to }) => {
   const scale = `scale(${scaleX}, ${scaleY})`;
 
   const rippleAnimationKeyframes = new KeyframeEffect(
-    to,
-    [
+    to, [
       {"transform": `${translate} scale(0)`},
       {"transform": `${translate} ${scale}`}
-    ],
-    {
+    ], {
       duration: 500,
       easing: "cubic-bezier(0.4, 0, 0.2, 1)",
       fill: "both"
@@ -135,12 +141,10 @@ const runHeroAnimation = ({ delay = 0, from, to }) => {
   const deltaHeight = fromRect.height / toRect.height;
 
   const heroAnimationKeyframes = new KeyframeEffect(
-    to,
-    [
+    to, [
       {"transform": `translate(${deltaLeft}px, ${deltaTop}px) scale(${deltaWidth}, ${deltaHeight})`},
       {"transform": "none"}
-    ],
-    {
+    ], {
       duration: 500,
       easing: "cubic-bezier(0.4, 0, 0.2, 1)",
       fill: "both",
@@ -151,7 +155,7 @@ const runHeroAnimation = ({ delay = 0, from, to }) => {
   to.style.transformOrigin = "0 0";
   to.style.zIndex = 10000;
   // TODO: no need and bad effect of black background behind the tile
-  // from.style.visibility = 'hidden';
+  // from.style.visibility = "hidden";
 
   const heroAnimation = new Animation(heroAnimationKeyframes, document.timeline);
   heroAnimation.play();
@@ -160,6 +164,65 @@ const runHeroAnimation = ({ delay = 0, from, to }) => {
     to.style.zIndex = "";
     from.style.visibility = "";
   });
+};
+
+const runFadeOutAnimation = ({ node }) => {
+  console.log('ray : ***** [runFadeOutAnimation]');
+  const fadeOutAnimationKeyframes = new KeyframeEffect(
+    node, [
+      {"opacity": "1"},
+      {"opacity": "0"}
+    ], {
+      duration: 500,
+      easing: "cubic-bezier(0.4, 0, 0.2, 1)",
+      // ray test touch <<
+      // TODO: tweak
+      // fill: "both"
+      fill: "backwards"
+      // ray test touch >>
+    }
+  );
+
+  const fadeOutAnimation = new Animation(fadeOutAnimationKeyframes, document.timeline);
+  fadeOutAnimation.play();
+
+  // ray test touch <<
+  fadeOutAnimation.onfinish = (() => {
+    console.log('ray : ***** [fadeOutAnimation.onfinish]');
+    toggleFullSizePageWithCard();
+  });
+  // ray test touch >>
+};
+
+const runTransformAnimation = ({
+  transformFrom = "none",
+  transformTo = "none",
+  node,
+  transformOrigin
+}) => {
+  const transformAnimationKeyframes = new KeyframeEffect(
+    node, [
+      {"transform": transformFrom},
+      {"transform": transformTo}
+    ], {
+      duration: 500,
+      easing: "cubic-bezier(0.4, 0, 0.2, 1)",
+      fill: "both"
+    }
+  );
+
+  if (transformOrigin) {
+    node.style.transformOrigin = transformOrigin;
+  }
+
+  const transformAnimation = new Animation(transformAnimationKeyframes, document.timeline);
+  transformAnimation.play();
+
+  // ray test touch <<
+  transformAnimation.onfinish = (() => {
+    console.log('ray : ***** [transformAnimation.onfinish]');
+  });
+  // ray test touch >>
 };
 
 for (const tile of tiles) {

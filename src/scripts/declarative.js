70f9aba5238e95fbd624b3715bbc23d24862624f
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-05-04 18:39:00 +0300

    feat: slide-from-right-animation & slide-left-animation

--- src/scripts/declarative.js
+++ src/scripts/declarative.js
@@ -40,18 +40,36 @@ showPage(selectedPageNumber);
 
 const slidePages = event => {
   const arrowButtonId = event.target.id;
+  let newSelectedPageNumber;
+  let entryNode;
 
+  const exitNode = document.getElementById(selectedPageNumber.toString());
   switch (arrowButtonId) {
     case BUTTON_IDS.PREV:
-      const nodeForSlideRightAnimation = document.getElementById(selectedPageNumber.toString());
-      const newSelectedPageNumber = selectedPageNumber === MIN_PAGE_NUMBER ? MAX_PAGE_NUMBER : (selectedPageNumber - 1);
-      const nodeForSlideFromLeftAnimation = document.getElementById(newSelectedPageNumber.toString());
+      newSelectedPageNumber = selectedPageNumber === MIN_PAGE_NUMBER ? MAX_PAGE_NUMBER : (selectedPageNumber - 1);
+      entryNode = document.getElementById(newSelectedPageNumber.toString());
       showPage(newSelectedPageNumber);
       setSelectedPageNumber(newSelectedPageNumber);
-      runSlideFromLeftAnimation({node: nodeForSlideFromLeftAnimation});
-      runSlideRightAnimation({node: nodeForSlideRightAnimation});
+      runSlideFromLeftAnimation({node: entryNode});
+      runSlideRightAnimation({
+        node: exitNode,
+        callback: () => {
+          hidePage(exitNode.id);
+        }
+      });
       break;
     case BUTTON_IDS.NEXT:
+      newSelectedPageNumber = selectedPageNumber === MAX_PAGE_NUMBER ? MIN_PAGE_NUMBER : (selectedPageNumber + 1);
+      entryNode = document.getElementById(newSelectedPageNumber.toString());
+      showPage(newSelectedPageNumber);
+      setSelectedPageNumber(newSelectedPageNumber);
+      runSlideFromRightAnimation({node: entryNode});
+      runSlideLeftAnimation({
+        node: exitNode,
+        callback: () => {
+          hidePage(exitNode.id);
+        }
+      });
       break;
     case BUTTON_IDS.UP:
       break;
@@ -61,7 +79,7 @@ const slidePages = event => {
   }
 };
 
-const runSlideFromLeftAnimation = ({ node, transformOrigin }) => {
+const runSlideFromLeftAnimation = ({ node, transformOrigin, callback }) => {
   const slideFromLeftAnimationKeyframes = new KeyframeEffect(
     node, [
       {"transform": "translateX(-100%)"},
@@ -82,12 +100,12 @@ const runSlideFromLeftAnimation = ({ node, transformOrigin }) => {
   slideFromLeftAnimation = new Animation(slideFromLeftAnimationKeyframes, document.timeline);
   slideFromLeftAnimation.play();
 
-  slideFromLeftAnimation.onfinish = (() => {
-    // TODO: do nothing for now
-  });
+  if (callback) {
+    slideFromLeftAnimation.onfinish = callback;
+  }
 };
 
-const runSlideRightAnimation = ({ node, transformOrigin }) => {
+const runSlideRightAnimation = ({ node, transformOrigin, callback }) => {
   const slideRightAnimationKeyframes = new KeyframeEffect(
     node, [
       {"transform": "none"},
@@ -108,10 +126,61 @@ const runSlideRightAnimation = ({ node, transformOrigin }) => {
   slideRightAnimation = new Animation(slideRightAnimationKeyframes, document.timeline);
   slideRightAnimation.play();
 
-  slideRightAnimation.onfinish = (() => {
-    // TODO: hide the page (removing the corresponding class)
-    hidePage(node.id);
-  });
+  if (callback) {
+    slideRightAnimation.onfinish = callback;
+  }
+};
+
+const runSlideFromRightAnimation = ({ node, transformOrigin, callback }) => {
+  const slideFromRightAnimationKeyframes = new KeyframeEffect(
+    node, [
+      {"transform": "translateX(100%)"},
+      {"transform": "none"}
+    ], {
+      duration: 500,
+      easing: "cubic-bezier(0.4, 0, 0.2, 1)",
+      fill: "both"
+    }
+  );
+
+  if (transformOrigin) {
+    node.style.transformOrigin = transformOrigin;
+  } else {
+    node.style.transformOrigin = "0 50%";
+  }
+
+  slideFromRightAnimation = new Animation(slideFromRightAnimationKeyframes, document.timeline);
+  slideFromRightAnimation.play();
+
+  if (callback) {
+    slideFromRightAnimation.onfinish = callback;
+  }
+};
+
+const runSlideLeftAnimation = ({ node, transformOrigin, callback }) => {
+  const slideLeftAnimationKeyframes = new KeyframeEffect(
+    node, [
+      {"transform": "none"},
+      {"transform": "translateX(-100%)"}
+    ], {
+      duration: 500,
+      easing: "cubic-bezier(0.4, 0, 0.2, 1)",
+      fill: "both"
+    }
+  );
+
+  if (transformOrigin) {
+    node.style.transformOrigin = transformOrigin;
+  } else {
+    node.style.transformOrigin = "0 50%";
+  }
+
+  slideLeftAnimation = new Animation(slideLeftAnimationKeyframes, document.timeline);
+  slideLeftAnimation.play();
+
+  if (callback) {
+    slideLeftAnimation.onfinish = callback;
+  }
 };
 
 for (const arrowButton of arrowButtons) {
